60bec0d9f209ad87a22560c2caa28df2ddc01c9f
mathcomercialeus-commits <mathcomercialeus@gmail.com>
2026-04-24 08:59:55 -0400

    Fix Postgres role migration

--- app.py
+++ app.py
@@ -127,23 +127,7 @@ def init_db():
     conn.execute("CREATE INDEX IF NOT EXISTS idx_rating_tokens_driver_id ON rating_tokens(driver_id);")
 
     if USE_POSTGRES:
-        conn.execute(
-            """
-            DO $$
-            DECLARE constraint_name text;
-            BEGIN
-                SELECT conname INTO constraint_name
-                FROM pg_constraint
-                WHERE conrelid = 'users'::regclass
-                  AND contype = 'c'
-                  AND pg_get_constraintdef(oid) LIKE '%role%';
-
-                IF constraint_name IS NOT NULL THEN
-                    EXECUTE format('ALTER TABLE users DROP CONSTRAINT %I', constraint_name);
-                END IF;
-            END $$;
-            """
-        )
+        conn.execute("ALTER TABLE users DROP CONSTRAINT IF EXISTS users_role_check;")
         conn.execute(
             """
             ALTER TABLE users
